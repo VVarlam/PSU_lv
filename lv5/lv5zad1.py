982ed8ba9e2380892c072e4064a2e1973de61a43
VVarlam <varlamvanesa@gmail.com>
2025-05-23 22:41:30 +0200

    Update lv5zad1.py

--- lv5/lv5zad1.py
+++ lv5/lv5zad1.py
@@ -2,21 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Učitavanje skupa podataka
 data_path = 'occupancy_processed.csv'
 data = pd.read_csv(data_path)
 print(f"Ukupno primjera u skupu podataka: {len(data)}")
 
-# Definicija značajki i ciljne varijable
 features = ['S3_Temp', 'S5_CO2']
 label = 'Room_Occupancy_Count'
 class_labels = {0: 'Slobodna', 1: 'Zauzeta'}
 
-# Pretvorba podataka u NumPy nizove
 X = data[features].to_numpy()
 y = data[label].to_numpy()
 
-# Vizualizacija podataka - dijagram raspršenja
 plt.figure(figsize=(8, 6))
 for class_val in np.unique(y):
     plt.scatter(
